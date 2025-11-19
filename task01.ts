3518c8d120760ab7a368413020c376a51f0da9e5
Pannnomaly <supawith007@gmail.com>
2025-11-19 15:17:57 +0700

    update: task01.ts and task02.ts

--- task01.ts
+++ task01.ts
@@ -1,3 +1,11 @@
 // TODO-01: Use if/else to print "Even" or "Odd"
 
-const number = 15;
+const number:number = 16;
+
+if (number % 2 === 0)
+{
+    console.log(number + " is Even.");
+} else
+{
+    console.log(number + " is Odd.");
+}
--- task02.ts
+++ task02.ts
@@ -1,5 +1,15 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+
+let isNumber = typeof userInput;
+
+if (isNumber === "number")
+{
+    console.log(userInput + " is a " + isNumber);
+} else
+{
+     console.log(userInput + " is not a number, it's a " + isNumber);
+}
