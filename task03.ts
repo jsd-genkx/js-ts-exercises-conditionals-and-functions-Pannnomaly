b2540363cc68e0961bf0c364d1938127b1554fac
Pannnomaly <supawith007@gmail.com>
2025-11-20 00:03:52 +0700

    update: task03-05.ts

--- task03.ts
+++ task03.ts
@@ -3,4 +3,23 @@
 // 6–10 chars → "Moderate"
 // 11+ chars → "Strong"
 
-const password = "pass123";
+const password:string = "12345678901";
+const passwordLength = password.length;
+// console.log(password);
+
+if (passwordLength > 10)
+{
+    console.log(`Your password "${password}" is Strong!`);
+} else if (passwordLength >= 6 && passwordLength <= 10)
+{
+    console.log(`Your password "${password}" is Moderate!`);
+} else
+{
+    console.log(`Your password "${password}" is Too short!`);
+}
+
+// แนวความคิดของโจทย์นี้คือ ต้องการตรวจสอบความยาวของรหัสผ่าน (password) และจะแสดงข้อความว่า password ที่เราตั้งมานั้น มีความแข็งแรงขนาดไหน โดยมีเงื่อนไขดังนี้
+// ถ้ารหัสผ่านมีความยาวน้อยกว่า 6 ตัวอักษร (default case) จะแสดงข้อความว่า "Too short"
+// ถ้ารหัสผ่านมีความยาวระหว่าง 6 ถึง 10 ตัวอักษร (passwordLength >=6 && passwordLength <= 10 คือจะต้องอยู่ในช่วง 6-10 ตัวอักษรเท่านั้น) จะแสดงข้อความว่า "Moderate"
+// ถ้ารหัสผ่านมีความยาวมากกว่า 10 ตัวอักษร จะแสดงข้อความว่า "Strong"
+// ซึ่งถ้า password ของเราเข้าเงื่อนไขสักอันมันจะแสดงผลของ condition นั้นออกมา แต่ถ้ายังไม่ใช่แล้วยังอยู่ในเงื่อนไขอื่น มันก็จะไปตรวจสอบเงื่อนไขถัดไปเรื่อยๆ จนกว่าจะเจอเงื่อนไขที่ตรงกับ password ของเราที่ตั้งไว้
--- task05.ts
+++ task05.ts
@@ -1,7 +1,7 @@
 // TODO-05: Convert this if to switch/case (You can add more options.)
 // TODO BONUS: Use object lookup
 
-const fruit: string = "banana";
+const fruit: string = "asd".trim().toLowerCase();
 
 if (fruit === "apple") {
 	console.log("An apple a day keeps the doctor away.");
@@ -12,3 +12,23 @@ if (fruit === "apple") {
 } else {
 	console.log("Unknown fruit.");
 }
+
+switch (fruit)
+{
+	case "apple":
+		console.log("An apple a day keeps the doctor away.");
+		break;
+	case "banana":
+		console.log("Banana is good for potassium.");
+		break;
+	case "orange":
+		console.log("Oranges are full of vitamin C.");
+		break;
+	default:
+		console.log("Unknown fruit.");
+}
+
+// แนวคิดของโจทย์นี้คือการเปลี่ยนโครงสร้างการควบคุมจาก if-else เป็น switch-case
+// เราใช้ switch-case ทำให้โค้ดดูสะอาดตาและง่ายต่อการอ่านมากขึ้นเมื่อมีหลายเงื่อนไข
+// โดยในแต่ละ case เราจะตรวจสอบค่าของตัวแปร fruit จะต้องตรงกันกับแต่ละ case ที่เรากำหนดไว้
+// เพื่อให้แสดงข้อความที่กำหนดไว้ตามผลลัพธ์ของแต่ละ case
