2b2a5f93366713f887ad45113dfa45c3388f7d81
Pannnomaly <supawith007@gmail.com>
2025-11-19 15:49:19 +0700

    update: task02.ts

--- task02.ts
+++ task02.ts
@@ -1,15 +1,19 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = NaN;
+const userInput = 42;
 // Try changing to "hello", 10, null, NaN
 
 let isNumber = typeof userInput;
 
-if (isNumber === "number")
+// Number.isNaN(userInput) ถ้า userInput เป็น NaN มันจะคืนค่า true แล้วเรา ! เพื่อให้เป็น false เพื่อให้เข้าเงื่อนไขสอง
+// isNaN(userInput) แบบนี้ก็ได้เหมือนกัน ไม่ต้องมี Number
+if (isNumber === "number" && !Number.isNaN(userInput))
 {
     console.log(userInput + " is a " + isNumber);
+    console.log(`The ${userInput} is a ${isNumber}`); // แบบ template literal
 } else
 {
-     console.log(userInput + " is not a number, it's a " + isNumber);
+     console.log(userInput + " is not a number");
+     console.log(`The ${userInput} is not a number`); // แบบ template literal
 }
